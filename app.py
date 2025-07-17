15f957cfaecc6186aa8ce8800f78631f4b75292b
Thia <thiafelicien3@gmail.com>
2025-07-17 09:40:42 +0300

    mise a jour

--- app.py
+++ app.py
@@ -502,7 +502,7 @@ def predict():
 # ----------------- Lancement de l'application -----------------
 if __name__ == '__main__':
     app.debug = True
-    app.run()
+    app.run(host='0.0.0.0',port=5000)
 
     
 
